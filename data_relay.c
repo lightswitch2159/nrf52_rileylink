eb53f60298e35b7e3e7487e131d0c54f90193c00
Pete Schwamb <pete@schwamb.net>
2020-10-31 22:23:38 -0500

    Skip first byte

--- data_relay.c
+++ data_relay.c
@@ -15,7 +15,9 @@ void data_relay_init(ble_rileylink_service_t * p_rileylink_service) {
 void data_relay_ble_write_handler(uint16_t conn_handle, ble_rileylink_service_t * p_rileylink_service, const uint8_t *data, uint16_t length)
 {
     NRF_LOG_INFO("Data received via BLE: %d bytes.", length);
-    run_command(data, length);
+    if (length >= 2) {
+        run_command(data+1, length-1);
+    }
 }
 
 void data_relay_spi_response_handler(const uint8_t *data, uint8_t length) {
